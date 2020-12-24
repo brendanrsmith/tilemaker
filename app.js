cdc44f7534e1a95cb9728489d75fad91cf0ab079
Brendan Smith <brendan5320@gmail.com>
2020-12-24 13:05:07 -0800

    day 2 starting code

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ box.addEventListener("click", () => {
 
 let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
-ctx.font = "30px Comic Sans MS";
+ctx.font = "25px Comic Sans MS";
 ctx.fillStyle = "coral";
 ctx.textAlign = "center";
 ctx.fillText("Hello World", canvas.width/2, canvas.height/2);
