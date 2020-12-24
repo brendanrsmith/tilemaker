431b5822ab0452f1bba25389cb224ad339163581
Brendan Smith <brendan5320@gmail.com>
2020-12-23 21:44:36 -0800

    added canvas js

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 const box = document.getElementById("box");
 box.addEventListener("click", () => {
-  box.style.backgroundColor = "blue";
+  if (box.style.backgroundColor != "blue") {
+      box.style.backgroundColor = "blue";
+  } else {
+    box.style.backgroundColor = "orange";
+  }
 });
+
+let canvas = document.getElementById("myCanvas");
+let ctx = canvas.getContext("2d");
+ctx.font = "30px Comic Sans MS";
+ctx.fillStyle = "coral";
+ctx.textAlign = "center";
+ctx.fillText("Hello World", canvas.width/2, canvas.height/2);
