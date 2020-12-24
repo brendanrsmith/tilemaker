64eda911e21e3084594ea2062479c5b3c9f50638
Brendan Smith <brendan5320@gmail.com>
2020-12-23 22:12:20 -0800

    last of the night

--- app.js
+++ app.js
@@ -13,3 +13,16 @@ ctx.font = "30px Comic Sans MS";
 ctx.fillStyle = "coral";
 ctx.textAlign = "center";
 ctx.fillText("Hello World", canvas.width/2, canvas.height/2);
+ctx.moveTo(75, 20);
+ctx.font = "30px Arial";
+ctx.strokeText("Hello World", 76, 22);
+ctx.lineTo(200, 100);
+ctx.stroke()
+
+ctx.globalAlpha = 0.2;
+ctx.fillStyle = "red";
+ctx.fillRect(20, 20, 75, 50);
+ctx.fillStyle = "blue";
+ctx.fillRect(50, 50, 75, 50);
+ctx.fillStyle = "green";
+ctx.fillRect(80, 80, 75, 50);
