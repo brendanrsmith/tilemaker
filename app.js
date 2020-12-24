22cb837c72d7b4df55f249da6ec79f6db4c695bb
Brendan Smith <brendan5320@gmail.com>
2020-12-24 15:18:59 -0800

    user params now draw mini tile

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ let ctx = canvas.getContext("2d");
 ctx.font = "25px Comic Sans MS";
 ctx.fillStyle = "coral";
 ctx.textAlign = "center";
-ctx.fillText("Hello World", canvas.width/2, canvas.height/2);
+ctx.fillText("placeholder", canvas.width/2, canvas.height/2);
 ctx.moveTo(75, 20);
 ctx.font = "30px Arial";
 ctx.strokeText("Hello World", 76, 22);
@@ -32,14 +32,14 @@ let sprite;
 // Draw tile canvas
 function startTile() {
   myTile.start();
-  sprite = new component(90, 90, "skyblue", 100, 120);
+  sprite = new component(100, 100, "skyblue", 100, 100);
 }
 
 let myTile = {
   canvas : document.createElement("canvas"),
   start : function() {
-    this.canvas.width = 500;
-    this.canvas.height = 500;
+    this.canvas.width = 200;
+    this.canvas.height = 200;
     this.context = this.canvas.getContext("2d");
     // Create target for myTile canvas to be drawn
     let target = document.getElementById("target");
@@ -55,6 +55,7 @@ function component(width, height, color, x, y) {
   this.width = width;
   this.height = height;
   this.angle = 0;
+  this.color = color;
   this.x = x;
   this.y = y;
   this.update = function() {
@@ -62,7 +63,7 @@ function component(width, height, color, x, y) {
     ctx.save();
     ctx.translate(this.x, this.y);
     ctx.rotate(this.angle);
-    ctx.fillStyle = color;
+    ctx.fillStyle = this.color;
     ctx.fillRect(this.width / -2, this.height / -2, this.width, this.height);
     ctx.restore();  }
 }
@@ -70,5 +71,8 @@ function component(width, height, color, x, y) {
 function updateTiles() {
   myTile.clear();
   sprite.angle = document.getElementById("rotate").value;
+  sprite.width = document.getElementById("user_size").value;
+  sprite.height = document.getElementById("user_size").value;
+  sprite.color = document.getElementById("user_color").value;
   sprite.update();
 }
